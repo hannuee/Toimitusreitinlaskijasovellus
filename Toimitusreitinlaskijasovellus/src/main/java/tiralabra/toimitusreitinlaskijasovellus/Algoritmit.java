ebdc7a5352d5ba985893df7b3de7674a51158325
Hannu Erälaukko <32358926+hannuee@users.noreply.github.com>
2018-02-10 01:54:06 +0200

    Toteutettu viimeinen ydinalgoritmi, kmDynaaminen.
    Tehty myös testejä generoiva funktio mutta ei saatu sitä vielä täysin toimimaan.

--- Toimitusreitinlaskijasovellus/src/main/java/tiralabra/toimitusreitinlaskijasovellus/Algoritmit.java
+++ Toimitusreitinlaskijasovellus/src/main/java/tiralabra/toimitusreitinlaskijasovellus/Algoritmit.java
@@ -62,7 +62,8 @@ public class Algoritmit {
      * @return Taulukko jossa ensimmäinen alkio kertoo lähtösolmun ja loput taulukon alkiot
      * kertovat solmun mihin taulukon edellisen alkion viittaamasta solmusta tulee edetä.
      */
-    public static int[] kauppamatkustajaBruteForce(int[][] verkko){
+    // Palauttaa toistaiseksi testausta varten vain parhaan reitin pituuden.
+    public static int kauppamatkustajaBruteForce(int[][] verkko){
         final int solmuja = verkko.length;  // Solmujen määrä verkossa.
 
         int[] paras = {Integer.MAX_VALUE};  // Lyhimmän reitin pituus.
@@ -92,105 +93,60 @@ public class Algoritmit {
         }
         reittiOhjeet[solmuja] = 0;  // Lisätään loppuun vielä ohje lähtösolmuun paluusta.
         
-        return reittiOhjeet;
+        return paras[0];
     }
     
     
+    // Otettu mallia:
+    // https://www.quora.com/Are-there-any-good-examples-of-the-Held-Karp-algorithm-in-C++-Hard-to-find-example-code-to-solve-the-traveling-salesman-problem-Everyone-wants-to-just-talk-about-theory-and-not-show-how-to-actually-do-it-What-is-the-big-secret
+    // Palauttaa toistaiseksi testausta varten vain parhaan reitin pituuden.
+    public static int kauppamatkustajaDynaaminen(int[][] verkko){
+        final int solmuja = verkko.length;  // Solmujen määrä verkossa.
+        
+        int[][] paras = new int[1<<(solmuja - 1)][solmuja];
+        int i = 0;
+        while(i < (1<<(solmuja - 1))){
+            int i2 = 0;
+            while(i2 < solmuja){
+                paras[i][i2] = Integer.MAX_VALUE;
+                ++i2;
+            }
+            ++i;
+        }
+        
+        for(int vierailtu = 1; vierailtu < (1<<(solmuja - 1)); ++vierailtu){
+            for(int viimeinen = 0; viimeinen < (solmuja - 1); ++viimeinen){
+                
+                if( (vierailtu & (1<<viimeinen)) == 0 ){
+                    continue;
+                }
+                
+                if(vierailtu == (1<<viimeinen)){
+                    paras[vierailtu][viimeinen] = verkko[solmuja - 1][viimeinen];
+                } else {
+                    int aiemminVierailtu = vierailtu ^ (1<<viimeinen);
+                    for(int aiempi = 0; aiempi < solmuja - 1; ++aiempi){
+                        if((aiemminVierailtu & (1<<aiempi)) == 0){
+                            continue;
+                        }
+                        paras[vierailtu][viimeinen] = Math.min(
+                                paras[vierailtu][viimeinen], 
+                                verkko[viimeinen][aiempi] + paras[aiemminVierailtu][aiempi]);
+                    }
+                }
+                
+            }
+        }
+        
+        int vastaus = Integer.MAX_VALUE;
+        for(int viimeinen = 0; viimeinen < solmuja - 1; ++viimeinen){
+            vastaus = Math.min(vastaus, verkko[viimeinen][solmuja - 1] + paras[(1<<(solmuja - 1)) - 1][viimeinen]);
+        }
+        
+        return vastaus;  
+    }
     
     
-    
-    
-//    /**
-//     * Tämä metodi on apumetodi metodille kauppamatkustajaDynaaminen.
-//     * Tämä metodi käy läpi kaikki reittivaihtoehdot rekursiivisesti
-//     * (paitsi ei niitä reittejä joista tulee varmasti pitempiä kuin siihenastisesta löydetystä lyhimmästä reitistä)
-//     * ja laittaa muistiin lyhimmän löytämänsä reitin.
-//     * @param pituus Tähänastisen reitin pituus
-//     * @param vierailtu Missä solmuissa on jo käyty ja milloin.
-//     * @param nykyinen Missä solmussa ollaan nyt.
-//     * @param monessako Monessako solmussa on jo vierailtu.
-//     * @param paras Lyhimmän reitin pituus.
-//     * @param parasReitti Lyhimmän löydetyn reitin vierailtu-taulukko.
-//     * @param solmuja Solmujen määrä verkossa.
-//     * @param verkko Verkko matriisimuodossa.
-//     * @see #kauppamatkustajaBruteForce(int[][] verkko)
-//     */
-//    private static void kmDynRekursio(int pituus, int[] vierailtu, int nykyinen, int monessako, 
-//                                     int[] paras, int[] parasReitti, final int solmuja, int[][] verkko){
-//        if(monessako == solmuja){  // JOS ollaan vierailtu kaikissa solmuissa NIIN tämä rekursiohaara loppuu. 
-//            if(pituus + verkko[nykyinen][0] < paras[0]){  // JA JOS reitti on tähänastisista reiteistä lyhin huomioiden matka solmuun 1
-//                paras[0] = pituus + verkko[nykyinen][0];  // NIIN laitetaan reitti muistiin.
-//                for(int i = 0; i < solmuja; i++){  // Kopioidaan paras tähänastinen reitti muistiin:
-//                    parasReitti[i] = vierailtu[i];
-//                }
-//            }
-//            return;
-//        }
-//        
-//        // Luuppi joka haarauttaa rekursion.
-//        for(int i = 1; i < solmuja; i++){
-//            // Nykyisestä solmusta(nykyinen) mennään jokaiseen ei-vierailtuun solmuun 
-//            // kunhan reitti ei silloin olisi jo lyhintä reittiä pitempi.
-//            if(vierailtu[i] == 0 && pituus + verkko[nykyinen][i] < paras[0]){    
-//                int[] vierailtu2 = vierailtu.clone();  // Kopioidaan vierailtu-taulukko uutta rekursiohaaraa varten.
-//                vierailtu2[i] = monessako + 1;  // Merkitään seuraava vierailtava solmu vierailluksi vierailujärjestysnumerolla. 
-//                kmDynRekursio(pituus + verkko[nykyinen][i], vierailtu2, i, monessako + 1, 
-//                             paras, parasReitti, solmuja, verkko);
-//            }
-//        }
-//    }
-//    
-//    //
-//    //
-//    /**
-//     * Palautta lyhimmän reitin joka käy verkon kaikki solmut läpi, alkaen solmusta 0 ja loppuen solmuun 0.
-//     * Lyhin reitti lasketaan käymällä läpi kaikki reittimahdollisuudet jotka alkavat solmusta 0 ja loppuvat solmuun 0.
-//     * Poikkeuksena reitit joista tulee varmasti pitempiä kuin siihenastisesta löydetystä lyhimmästä reitistä.
-//     * @param verkko Verkko matriisimuodossa. 
-//     * (Ulomman taulukon indexi kertoo mistä solmusta kaari lähtee, 
-//     * sisemmän taulukon indexi kertoo mihin solmuun kaari menee,
-//     * sisemmän taulukon indexiä vastaava arvo kertoo kuinka pitkä kyseinen suunnattu kaari on.)
-//     * @return Taulukko jossa ensimmäinen alkio kertoo lähtösolmun ja loput taulukon alkiot
-//     * kertovat solmun mihin taulukon edellisen alkion viittaamasta solmusta tulee edetä.
-//     */
-//    public static int[] kauppamatkustajaDynaaminen(int[][] verkko){
-//        final int solmuja = verkko.length;  // Solmujen määrä verkossa.
-//
-//        int[] paras = {Integer.MAX_VALUE};  // Lyhimmän reitin pituus.
-//        int[] parasReitti = new int[solmuja];  // Lyhimmän löydetyn reitin vierailtu-taulukko.
-//
-//        // Taulukko joka kertoo missä solmuissa on jo käyty ja milloin.
-//        // Taulukon indexi kertoo solmun, 
-//        // taulukon indexiä vastaava arvo kertoo milloin solmussa on vierailtu.
-//        // (0=ei vierailtu, 1=vierailtu ekana, 2=vierailtu tokana, ...)
-//        int[] vierailtu = new int[solmuja];
-//        // Alustetaan vierailtu-taulukko.
-//        for(int i = 0; i < solmuja; i++){
-//            vierailtu[i] = 0;
-//        }
-//        vierailtu[0] = 1;
-//        
-//        // Käydään rekursion avulla läpi reittivaihtoehdot.
-//        kmDynRekursio(0, vierailtu, 0, 1, 
-//                     paras, parasReitti, solmuja, verkko);
-//        
-//        // Luodaan reittiOhjetaulukko parasReitti-taulukon avulla.
-//        int[] reittiOhjeet = new int[solmuja + 1];
-//        int i = 0;
-//        while(i < solmuja){
-//            reittiOhjeet[parasReitti[i] - 1] = i;
-//            ++i;
-//        }
-//        reittiOhjeet[solmuja] = 0;  // Lisätään loppuun vielä ohje lähtösolmuun paluusta.
-//        
-//        return reittiOhjeet;
-//    }
-    
-    
-    
-    
-    
-    // erikoistapaukset??
     public static int[] kauppamatkustajaHeuristinen(int[][] verkko){    // IDEA: wikistä, lähin naapuri taktiikka. IMPLEMENTAATIO: itse.
         final int solmuja = verkko.length;  // Solmujen määrä verkossa.
         
--- Toimitusreitinlaskijasovellus/src/test/java/tiralabra/toimitusreitinlaskijasovellus/MainTest.java
+++ Toimitusreitinlaskijasovellus/src/test/java/tiralabra/toimitusreitinlaskijasovellus/MainTest.java
@@ -1,19 +1,81 @@
 /**
- *
- * @author Hannu
+ *@author Hannu Erälaukko
  */
 package tiralabra.toimitusreitinlaskijasovellus;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;      // Testit joissa testataan vaan että kaikki solmut käyty läpi tasan kerran, random taulukkoja?
+import java.util.Random;
 
-public class MainTest { 
+import static org.junit.Assert.*;
+
+public class MainTest {
     
-    // Kopioitu härskisti syksy 2017 TIRA_08_01 tehtävästä testiverkot e1-e4 kun
-    // ei vielä ehditty tehdä omia testiverkkoja. (Ja vastaukset jotka pitäisi saada näillä testiverkoilla.)
-    //@Test
+    // Funktio joka arpoo asymmetrisen etäisyysmatriisin verkolle jossa on solmuja parametrinaan saaman luvun verran.
+    // Etäisyydet verkon solmujen välillä tulevat olemaan välillä 1 - 1000.
+    public static int[][] verkonArpoja(int solmuja){
+        Random arpoja = new Random();
+        
+        int[][] verkko = new int[solmuja][solmuja];
+        int i = 0;
+        while(i < solmuja){
+            int i2 = 0;
+            while(i2 < solmuja){
+                if(i == i2){  // Koska etäisyys jostakin solmusta itseensä on 0.
+                    verkko[i][i2] = 0;
+                } else {
+                    verkko[i][i2] = arpoja.nextInt(1000) + 1;
+                }
+                ++i2;
+            }
+            ++i;
+        }
+        
+        return verkko;
+    }
+    
+    // Funktio joka tarkistaa että sen parametrinaan saaman vastaus-taulukon
+    // ensimmäinen ja viimeinen alkio on 0 ja että taulukossa on lukuja väliltä
+    // 1 - (solmuja-1) tasan yksi jokaista.
+    public static boolean vastauksenTarkistaja(int solmuja, int[] vastaus){
+        if(vastaus.length != solmuja + 1){  // Tarkistetaan että vastaus-taulukko on oikean pituinen.
+            return false;
+        }
+        if(vastaus[0] != 0 || vastaus[solmuja] != 0){  // Ensimmäinen ja viimeinen alkio on 0.
+            return false;
+        }
+        
+        int[] tarkistin = new int[solmuja];
+        int i = 0;
+        while(i < solmuja){
+            tarkistin[i] = 0;
+            ++i;
+        }
+        
+        // Käydään vastaus-taulukko läpi poislukien ensimmäinen ja viimeinen indexi,
+        // ja pidetään tarkistin-taulukolla kirjaa montako kertaa kussakin solmussa on vierailtu.
+        i = 1;
+        while(i < vastaus.length - 1){
+            ++tarkistin[vastaus[i]];
+            ++i;
+        }
+        
+        // Tarkastetaan tarkistin-taulukko:
+        i = 1;
+        while(i < solmuja){
+            if(tarkistin[i] != 1){
+                return false;
+            }
+            ++i;
+        }
+        
+        return true;
+    }
+    
+    
+    // Kopioitu härskisti syksy 2017 TIRA_08_01 tehtävästä testiverkot e1-e4.
+    @Test
     public void kauppamatkustajaBruteForceTestit(){
         int[][] e1={{0,3,2,1},
                     {3,0,4,2},
@@ -40,6 +102,33 @@ public class MainTest {
         assertEquals(14, Algoritmit.kauppamatkustajaBruteForce(e4));
      }
     
+    @Test
+    public void kauppamatkustajaDynaaminenTestit(){
+        int[][] e1={{0,3,2,1},
+                    {3,0,4,2},
+                    {2,4,0,4},
+                    {1,2,4,0}};
+        assertEquals(9, Algoritmit.kauppamatkustajaDynaaminen(e1));
+        
+        int[][] e2={{0,2,1,1},
+                    {2,0,1,1},
+                    {1,1,0,2},
+                    {1,1,2,0}};
+        assertEquals(4, Algoritmit.kauppamatkustajaDynaaminen(e2));
+        
+        int[][] e3={{0,1,2,1},
+                    {1,0,1,2},
+                    {2,1,0,1},
+                    {1,2,1,0}};
+        assertEquals(4, Algoritmit.kauppamatkustajaDynaaminen(e3));
+        
+        int[][] e4={{0,1,2,3},
+                    {1,0,4,5},
+                    {2,4,0,6},
+                    {3,5,6,0}};
+        assertEquals(14, Algoritmit.kauppamatkustajaDynaaminen(e4));
+    }
+    
     // Nämä testit tehty itse
     @Test
     public void kauppamatkustajaHeuristinenTestit(){
@@ -47,19 +136,19 @@ public class MainTest {
                     {5,5,5,5},
                     {2,5,5,5},
                     {5,5,1,5}};
-        assertArrayEquals(new int[]{0,1,3,2,0}, Algoritmit.kauppamatkustajaBruteForce(m1));
+        assertArrayEquals(new int[]{0,1,3,2,0}, Algoritmit.kauppamatkustajaHeuristinen(m1));
         
         int[][] m2={{0,2,5,5},
                     {5,5,5,5},
                     {1,5,5,5},
                     {5,5,1,5}};
-        assertArrayEquals(new int[]{0,1,3,2,0}, Algoritmit.kauppamatkustajaBruteForce(m2));
+        assertArrayEquals(new int[]{0,1,3,2,0}, Algoritmit.kauppamatkustajaHeuristinen(m2));
         
         int[][] m3={{0,5,5,1},
                     {5,5,3,5},
                     {2,5,5,5},
                     {2,5,5,5}};
-        assertArrayEquals(new int[]{0,3,1,2,0}, Algoritmit.kauppamatkustajaBruteForce(m3));
+        assertArrayEquals(new int[]{0,3,1,2,0}, Algoritmit.kauppamatkustajaHeuristinen(m3));
     }
     
 }
